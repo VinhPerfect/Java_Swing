69fc6bce9738f1df18825c1bf57abe84e26a0000
VinhPerfect <lqv.209@gmail.com>
2024-12-17 21:59:25 +0700

    Java_Swing

---
+++ src/swingcunganhvinh/SwingcunganhVinh.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package swingcunganhvinh;
+
+/**
+ *
+ * @author lqv20
+ */
+public class SwingcunganhVinh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        }
+    
+}
